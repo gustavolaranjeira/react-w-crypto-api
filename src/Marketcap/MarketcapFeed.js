f757dbd6b77696920e459ca9a66cd136449f58d8
Gustavo Laranjeira <gustavolaranjeiramo@outlook.com>
2021-06-24 19:57:50 -0300

    adding marketcapfeed

---
+++ src/Marketcap/MarketcapFeed.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+//┌―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――┐//
+//│   PROGRAMA  => Marketcap.js     │   AUTOR => Gustavo Laranjeira   │      DATA => 24/06/2021
+//├―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――┤//
+//│ DESCRICAO =>  Descrição - Get cryptocurrency global data top10
+//│
+//├―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――┤//
+//│                                  HISTORICO DE MANUTENCAO
+//├―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――┤//
+//│ AUTOR => __________ │ DESCRICAO =>
+//├―――――――――――――――――――――┤
+//│ DATA  => dd/mm/aaaa │
+//└―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――┘//
+
+const MarketcapFeed = () => {
+  const [marketcap, setMarketcap] = React.useState([]);
+  React.useEffect(() => {
+    async function getMarketcap() {
+      const response = await fetch('https://api.coingecko.com/api/v3/global', {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+        },
+      });
+      console.log('response', response);
+      const responseJson = await response.json();
+      console.log('responseJson', responseJson);
+      console.log('responseJson.data', responseJson.data);
+      console.log(
+        'responseJson.data.market_cap_percentage',
+        responseJson.data.market_cap_percentage,
+      );
+      setMarketcap(responseJson.data.market_cap_percentage);
+    }
+    getMarketcap();
+  }, []);
+
+  return (
+    <>
+      <h6 style={{ fontSize: 'larger', marginBottom: '0px' }}>
+        Market Cap Top10
+      </h6>
+      {marketcap && (
+        <ol style={{ padding: '0px', marginTop: '0px' }}>
+          {Object.keys(marketcap).map((coin, index) => (
+            <>
+              <li>{Object.keys(marketcap)[index]}</li>
+              <ul style={{ padding: '0px', marginTop: '0px' }}>
+                <li style={{ listStyle: 'none' }}>
+                  {Object.values(marketcap)[index]}
+                </li>
+              </ul>
+            </>
+          ))}
+        </ol>
+      )}
+    </>
+  );
+};
+
+export default MarketcapFeed;
